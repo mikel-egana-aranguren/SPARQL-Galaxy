5b744d4c7a3e58b714d19b290e619110e8a388e4
mikel-egana-aranguren <mikel.egana.aranguren@gmail.com>
2014-04-23 17:20:40 +0200

    Rewritten using RDFLib instead of JENA

---
+++ galaxy-dist/tools/sparql_galaxy/sparql.py
@@ -0,0 +1,27 @@
+
+
+import sys
+import rdflib
+
+def main(argv):
+
+    g = rdflib.Graph()
+    g.parse(argv[1])
+    results = g.query(argv[2])
+    print "<table>"
+    print "<tr>"
+    for key in results.bindings[1].keys():
+        print "<td><b>" + key +"</b></td>"
+    print "</tr>"
+    for binding in results.bindings:
+        print "<tr>"
+        for value in binding.values():  
+            if isinstance(value, rdflib.term.URIRef):
+                print "<td><a href=\"" + value +"\">"+ value +"</a></td>"
+            else:
+                print "<td>"+ value +"</td>"
+        print "</tr>"
+    print "</table>"
+    
+if __name__ == "__main__":
+    main(sys.argv)
