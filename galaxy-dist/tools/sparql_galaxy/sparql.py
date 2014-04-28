e0e3c7419de5bed08149a8378ac74b988b666901
mikel-egana-aranguren <mikel.egana.aranguren@gmail.com>
2014-04-28 16:09:52 +0200

    HTML or tabular output added

--- galaxy-dist/tools/sparql_galaxy/sparql.py
+++ galaxy-dist/tools/sparql_galaxy/sparql.py
@@ -1,13 +1,21 @@
 
-
 import sys
 import rdflib
+from operator import eq
 
 def main(argv):
 
     g = rdflib.Graph()
     g.parse(argv[1])
     results = g.query(argv[2])
+    render = argv[3]
+
+    if (render == "html"):
+        html_render(results)
+    else:
+        tab_render(results)
+    
+def html_render(results):
     print "<table>"
     print "<tr>"
     for key in results.bindings[1].keys():
@@ -23,5 +31,14 @@ def main(argv):
         print "</tr>"
     print "</table>"
     
+def tab_render(results):
+    for key in results.bindings[1].keys():
+        print key + "\t",
+    print "\n"
+    for binding in results.bindings:
+        for value in binding.values():  
+            if isinstance(value, rdflib.term.URIRef):
+                print value + "\t",
+
 if __name__ == "__main__":
     main(sys.argv)
